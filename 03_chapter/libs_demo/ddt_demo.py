de2a589a7cf0be3a36499e7bef5cfc0c9c891a96
defnngj <defnngj@gmail.com>
2024-03-02 12:01:10 +0800

    chapter 03

---
+++ 03_chapter/libs_demo/ddt_demo.py
@@ -0,0 +1,28 @@
+# ddt_demo.py
+import unittest
+from ddt import ddt, data, unpack
+
+
+@ddt
+class TestDTT(unittest.TestCase):
+
+    @data([1, 2, 3], [4, 5, 9], [6, 7, 13])
+    @unpack
+    def test_add_list(self, a, b, c):
+        self.assertEqual(a+b, c)
+
+    @data(("Hi", "HI"), ("hello", "HELLO"), ("world", "WORLD"))
+    @unpack
+    def test_upper_tuple(self, s1, s2):
+        self.assertEqual(s1.upper(), s2)
+
+    @data({"d": "hello world", "t": str},
+          {"d": 110, "t": int},
+          {"d": True, "t": bool})
+    @unpack
+    def test_data_type_dict(self, d, t):
+        self.assertTrue(isinstance(d, t))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
